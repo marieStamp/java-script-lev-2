7319dabf57017650fc5c346d3243187284e43cc7
marieStamp <mariemarkina@gmail.com>
2021-07-29 16:59:47 +0300

    hw2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,71 @@
-const products = [
-    {id: 1, title: 'Notebook Xiaomi', price: 2000, image: 'notebook.jpg'},
-    {id: 2, title: 'Mouse Apple', price: 20, image: 'mouse.jpg'},
-    {id: 3, title: 'Keyboard Apple', price: 200, image: 'keyboard.jpg'},
-    {id: 4, title: 'Gamepad Sony', price: 50, image: 'gamepad.jpg'},
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook Xiaomi', price: 2000, img: 'notebook.jpg' },
+            { id: 2, title: 'Mouse Apple', price: 20, img: 'mouse.jpg' },
+            { id: 3, title: 'Keyboard Apple', price: 200, img: 'keyboard.jpg' },
+            { id: 4, title: 'Gamepad Sony', price: 50, img: 'gamepad.jpg' },
+        ];
+    }
 
-const pathToImages = 'img';
-const pathToProductsImages = `${pathToImages}/catalogue`;
-const productsEl = document.querySelector('.products');
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
 
-const renderProduct = (product) => {
-    return `
-    <div class="item">
-        <img class="item_img" src="${pathToProductsImages}/${product.image}" alt="${product.title}>
-        <p class="itemName">${product.title}</p>
-        <p class="itemPrice">$${product.price}</p>
-        <button class="btn-cart" type="button">Add to Cart</button>
-    </div>
-    `
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item)).join('');
-    productsEl.insertAdjacentHTML('afterbegin', productsList);
-};
+const pathToProductsImg = 'img/catalogue';
 
-renderPage(products);
+class ProductItem {
+    constructor(product) {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = product.img;
+    }
+
+    render() {
+        return `
+        <div class="item">
+            <img class="item_img" src="${pathToProductsImg}/${this.img}">
+            <p class="itemName">${this.title}</p>
+            <p class="itemPrice">$${this.price}</p>
+            <button class="btn-cart" type="button">Add to Cart</button>
+        </div>
+        `
+    };
+}
+
+let list = new ProductList();
+
+class Cart {
+
+    addItem() { } // метод добавляет товар в корзину 
+    removeItem() { } // метод удаляет товар из корзины 
+    saveCart() { } // метод сохраняет данные в корзине 
+    clearCart() { } // метод удаляет все данные из корзины 
+    getById() { } // метод ищет элемент корзины по id товара 
+    makeOrder() { } // метод отправляет заказ в обработку 
+    renderCart() { } // метод отрисовывает все товары корзины
+
+    getSum() {    // метод рассчитывает общую стоимостль товаров в корзине
+        let s = 0;
+        this.goods.forEach(item => {
+            s += item.price;
+        })
+    }
+}
+
+class CartItem {
+
+    renderCartItem() { } // метод отрисовывает товар корзины
+}
