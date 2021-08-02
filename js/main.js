9554c0cb8dd63a7081e908a80936a608f33d895b
marieStamp <mariemarkina@gmail.com>
2021-08-02 14:30:49 +0300

    hw3

--- js/main.js
+++ js/main.js
@@ -1,44 +1,60 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
-        this._fetchProducts();
-        this.render();
+        this._getProducts()
+            .then(data => { 
+                this.goods = [...data];
+                this.render()
+       });
     }
-    _fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'Notebook Xiaomi', price: 2000, img: 'notebook.jpg' },
-            { id: 2, title: 'Mouse Apple', price: 20, img: 'mouse.jpg' },
-            { id: 3, title: 'Keyboard Apple', price: 200, img: 'keyboard.jpg' },
-            { id: 4, title: 'Gamepad Sony', price: 50, img: 'gamepad.jpg' },
-        ];
-    }
+    
+//     _fetchProducts() {
+//         this.goods = [
+//             { id: 1, title: 'Notebook Xiaomi', price: 2000, img: 'notebook.jpg' },
+//             { id: 2, title: 'Mouse Apple', price: 20, img: 'mouse.jpg' },
+//             { id: 3, title: 'Keyboard Apple', price: 200, img: 'keyboard.jpg' },
+//             { id: 4, title: 'Gamepad Sony', price: 50, img: 'gamepad.jpg' },
+//         ];
+//     }
 
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
     render() {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
-            const item = new ProductItem(product);
-            block.insertAdjacentHTML("beforeend", item.render());
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", productObj.render());
         }
     }
 }
 
-const pathToProductsImg = 'img/catalogue';
+//const pathToProductsImg = 'img/catalogue';
 
 class ProductItem {
-    constructor(product) {
-        this.id = product.id;
-        this.title = product.title;
+    constructor(product, img = 'img/catalogue') {
+        this.id = product.id_product;
+        this.title = product.product_name;
         this.price = product.price;
-        this.img = product.img;
+        this.img = img;
     }
 
     render() {
         return `
         <div class="item">
-            <img class="item_img" src="${pathToProductsImg}/${this.img}">
+            <img class="item_img" src="${this.img}/${this.id}.jpg">
             <p class="itemName">${this.title}</p>
-            <p class="itemPrice">$${this.price}</p>
+            <p class="itemPrice">₽${this.price}</p>
             <button class="btn-cart" type="button">Add to Cart</button>
         </div>
         `
@@ -48,24 +64,61 @@ class ProductItem {
 let list = new ProductList();
 
 class Cart {
-
-    addItem() { } // метод добавляет товар в корзину 
-    removeItem() { } // метод удаляет товар из корзины 
-    saveCart() { } // метод сохраняет данные в корзине 
-    clearCart() { } // метод удаляет все данные из корзины 
-    getById() { } // метод ищет элемент корзины по id товара 
-    makeOrder() { } // метод отправляет заказ в обработку 
-    renderCart() { } // метод отрисовывает все товары корзины
-
-    getSum() {    // метод рассчитывает общую стоимостль товаров в корзине
-        let s = 0;
-        this.goods.forEach(item => {
-            s += item.price;
-        })
+    constructor(container = '.dropdown__cart') {
+        this.container = container;
+        this.goods = [];
+        this._getCartItem()
+            .then(data => {
+                this.goods = [...data.contents];
+                this.render()
+            });
     }
+
+    _getCartItem(){
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new CartItem();
+            block.insertAdjacentHTML("beforeend", productObj.renderCartItem(product));
+        }
+    }
+
+
+    // addItem() { } // метод добавляет товар в корзину 
+    // removeItem() { } // метод удаляет товар из корзины 
+    // saveCart() { } // метод сохраняет данные в корзине 
+    // clearCart() { } // метод удаляет все данные из корзины 
+    // getById() { } // метод ищет элемент корзины по id товара 
+    // makeOrder() { } // метод отправляет заказ в обработку 
+    // renderCart() { } // метод отрисовывает все товары корзины
+
+    // getSum() {    // метод рассчитывает общую стоимость товаров в корзине
+    //     let s = 0;
+    //     this.goods.forEach(item => {
+    //         s += item.price;
+    //     })
+    // }
 }
 
 class CartItem {
 
-    renderCartItem() { } // метод отрисовывает товар корзины
+    renderCartItem(product) { 
+        return `<div class="cartRow">
+                    <div>${product.product_name}</div>
+                    <div>шт.${product.quantity}</div>
+                    <div>₽${product.price}</div>  
+                    <div class="dropdown__cart_action">
+                            <a href="#"><i class="fas fa-times-circle" aria-hidden="true"></i></a>
+                        </div>
+                 </div>
+        `
+    } 
 }
+
+new Cart();
